b9e056f78e4b8cfe9a5e27a680ae86e2f5c907c7
Ahmad Shahid <ahmadbilal3158@gmail.com>
2024-08-12 13:47:32 +0500

    Collaped sidePanel created

--- src/components/Prenav2.jsx
+++ src/components/Prenav2.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
 // Icons import code below
 import { CiSearch } from "react-icons/ci";
 import { CiUser } from "react-icons/ci";
 import { CiHeart } from "react-icons/ci";
 import { CiShoppingCart } from "react-icons/ci";
 import { Link } from 'react-router-dom';
+import Collapsedsidepanel from './Collapsedsidepanel';
 
 const Prenav2 = () => {
   return (
     <div className='prenav2'>
-        <div className="empty"></div>
+        <div className="collapsedsidepanel"><Collapsedsidepanel/></div>
        <div className="logo"><img src="https://stylo.pk/cdn/shop/files/110x60-Logo_45_140x.png?v=1705411679" /></div>
         <div className="icons">
             <div className="icon1"><CiSearch /></div> 
